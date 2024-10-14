8f40ccf7d95498bac16c030405bb4f111ff1c329
Muhamad Luthfi Bangun Permadi <luthfinewzoneonline@gmail.com>
2024-10-14 13:25:13 +0700

    #4 add projects cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 // import Image from "next/image";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -11,9 +11,7 @@ export default function Home() {
      flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-        navItems={[
-          {name: "Home", link: "/", icon: <FaHome/>},
-        ]}
+        navItems={navItems}
         />
         <Hero/>
         <Grid/>
